2c352f52c32b2722a472e3b96e091a7ea31ef38d
panucitelcz <petr.b257@seznam.cz>
2024-04-09 11:53:20 +0200

    Header komplet hotovo

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -28,4 +28,26 @@ hamburger.addEventListener("click", function () {
 
       state = true; //Zde měním state na true, aby po druhý proběhl opačný proces, tím se nám navigace bude posouvat nahoru a dolů a hamburer rozevírat a zavírat
     }
-}); //Díky tomuto může fungovat animace, když se přidává nebo odebírá přidaná třída do elementu.
+}); //Zde založíme funkci, která reaguje na změnu šířky okna a kontoluje kdy se odebere classa
+//ControlWidth() je název funkce bez parametru
+
+function ControlWidth() {
+  var sirka = window.innerWidth; //Deklarace proměný, kde se ukládá šířka okna
+  //Podmínka která kontroluje šířku okna (celé stránky)
+  //Díky tomu pokud někdo zvětší šířku stránky a bude otevřený hamburger s navigací, tohle zařídí aby se classy odebrali)
+
+  if (sirka < 768) {
+    hamburger.classList.remove("is-active"); //Odebere classu s hamburgeru .is-active
+
+    nav.classList.remove("is-open"); //Odebere classu s navigace
+
+    state = false; //nastavíme zpět na defaultní hodnotu state pro kontrolu pro hamburger
+  }
+} //Tato funkce volá funkci, pokud se něco změní na stránce, způsobu je více. Tohle je jedna z nich.
+//window - objekt který prezentuje okno prohlížeče
+//addEventListener - přidává událost a naslouchá
+//resize - spustí se, pokud se změní velikost okna
+//ControlWidth() - spustí se funkce, kterou jsme vytvořili
+
+
+window.addEventListener('resize', ControlWidth()); //Díky tomuto může fungovat animace, když se přidává nebo odebírá přidaná třída do elementu.
--- script/script.js
+++ script/script.js
@@ -16,6 +16,7 @@ hamburger.addEventListener("click", () => {
         nav.classList.remove("is-open"); //To samé jako v horním řádku, ale pro nav
         state = false; //Nastavujeme zpět na false, aby se nestalo, že proběhné příkaz dvakrát po sobě. Díky tomu funguje tato podmínka. Pozor, musíte pokažde kliknout na hamburger, aby to fungovalo
     } 
+    
     //Pokud state je false, splní se tato podmínka
     else {
         hamburger.classList.add("is-active"); //Volám proměnou hamburger, která odkazuje na html. Používám classlist.add - přidávám do elementu další classu, kterou jsem si pojmenoval "is-active". Tuto třídu my upravíme a podle toho se projeví animace
@@ -23,5 +24,28 @@ hamburger.addEventListener("click", () => {
         state = true; //Zde měním state na true, aby po druhý proběhl opačný proces, tím se nám navigace bude posouvat nahoru a dolů a hamburer rozevírat a zavírat
     }
 })
+
+//Zde založíme funkci, která reaguje na změnu šířky okna a kontoluje kdy se odebere classa
+//ControlWidth() je název funkce bez parametru
+function ControlWidth() {
+    const sirka = window.innerWidth; //Deklarace proměný, kde se ukládá šířka okna
+    
+    //Podmínka která kontroluje šířku okna (celé stránky)
+    //Díky tomu pokud někdo zvětší šířku stránky a bude otevřený hamburger s navigací, tohle zařídí aby se classy odebrali)
+    if (sirka < 768)
+    {
+        hamburger.classList.remove("is-active"); //Odebere classu s hamburgeru .is-active
+        nav.classList.remove("is-open"); //Odebere classu s navigace
+        state = false; //nastavíme zpět na defaultní hodnotu state pro kontrolu pro hamburger
+    }
+
+}
+//Tato funkce volá funkci, pokud se něco změní na stránce, způsobu je více. Tohle je jedna z nich.
+//window - objekt který prezentuje okno prohlížeče
+//addEventListener - přidává událost a naslouchá
+//resize - spustí se, pokud se změní velikost okna
+//ControlWidth() - spustí se funkce, kterou jsme vytvořili
+window.addEventListener('resize', ControlWidth());
+
 
 //Díky tomuto může fungovat animace, když se přidává nebo odebírá přidaná třída do elementu.
